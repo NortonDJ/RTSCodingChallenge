8815403572eea5e699a358885b49e51e31fcb5f8
nortondj <nortondj@lafayette.edu>
2018-08-22 23:51:43 -0700

    Add tests for Problem 2

---
+++ src/test/scala/solutions/problem2/StringRotatorTest.scala
@@ -0,0 +1,19 @@
+package solutions.problem2
+
+import org.scalatest.FunSuite
+
+class StringRotatorTest extends FunSuite {
+
+  test("testRotate") {
+    assert(StringRotator.rotate("MyString", 2) === "ngMyStri")
+    assert(StringRotator.rotate("MyString", 0) === "MyString")
+    assert(StringRotator.rotate("MyString", 8) === "MyString")
+    assert(StringRotator.rotate("", 0) === "")
+    assert(StringRotator.rotate("", 5) === "")
+    assert(StringRotator.rotate(null, 5) === null)
+    assert(StringRotator.rotate("MyString", -1) === "yStringM")
+    assert(StringRotator.rotate("MyString", -8) === "MyString")
+    assert(StringRotator.rotate("MyString", -4) === "ringMySt")
+  }
+
+}
