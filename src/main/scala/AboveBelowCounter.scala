769e04d50ba1c88fe696c28831e221589b036415
nortondj <nortondj@lafayette.edu>
2018-08-22 23:14:48 -0700

    Add doc for problem 1

--- src/main/scala/AboveBelowCounter.scala
+++ src/main/scala/AboveBelowCounter.scala
@@ -1,15 +1,21 @@
 object AboveBelowCounter {
 
   /**
+    * Given an array and a pivot, this method calculates two counts:
+    * 1. the number of elements above the pivot,
+    * 2. and the number of elements below the pivot
     *
-    * @param array
-    * @param pivot
-    * @return
+    * @param array an Array of Ints
+    * @param pivot the pivot
+    * @return (number of elements above the pivot, number of elements below the pivot)
     */
   def count(array: Array[Int], pivot: Int): (Int, Int) = {
     array match {
+      // handle the case where the user passes null
       case null => (0, 0)
-      case list => list.foldLeft((0, 0)) {
+
+      // otherwise the input is valid
+      case values => values.foldLeft((0, 0)) {
         case ((above, below), elem) =>
           if (elem < pivot)
             (above, below + 1)
--- src/main/scala/Problem1.scala
+++ src/main/scala/Problem1.scala
@@ -1,4 +1,10 @@
+/**
+  * #1  Print the number of integers in an array that are above the given input
+  * and the number that are below.
+  *
+  * e.g. for the array [1, 5, 2, 1, 10] with input 6, print “above: 1, below: 4”.
+  */
 object Problem1 extends App {
   val (above, below) = AboveBelowCounter.count(Array(1, 5, 2, 1, 10), 6)
-  println(s"above: ${above}, below: ${below}")
+  println(s"above: $above, below: $below")
 }
