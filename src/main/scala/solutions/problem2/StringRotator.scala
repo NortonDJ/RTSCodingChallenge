7dd74cfa013bfdf01eb37ddf912478270299b61e
nortondj <nortondj@lafayette.edu>
2018-08-23 00:10:38 -0700

    Add doc for String Rotation

--- src/main/scala/solutions/problem2/StringRotator.scala
+++ src/main/scala/solutions/problem2/StringRotator.scala
@@ -1,13 +1,24 @@
 package solutions.problem2
 
 object StringRotator {
+
+  /**
+    * Given a String input, and an amount to shift by, this method
+    * returns a String such that the characters are shifted to the right,
+    * with attention to overflow being shifted to the front of the string.
+    *
+    * @param input the String to rotate
+    * @param shift the amount to shift by; if this value is negative, then we shift
+    *              with overflow to the left
+    * @return the shifted String
+    */
   def rotate(input: String, shift: Int): String = {
     def magnitudeOfShift(str: String, shift: Int) = shift % str.length
 
     def indexFromTheRight(str: String, indexFromTheLeft: Int) =
       str.length - indexFromTheLeft
 
-    def applyShift(str: String, index: Int): String = {
+    def applyShift(str: String, index: Int) = {
       val (left, right) = str.splitAt(index)
       right.concat(left)
     }
